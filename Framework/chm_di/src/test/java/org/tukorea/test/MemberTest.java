d05d1a53d419c0ea10b6494b298f45b499740464
papavhub <pavi03@kpu.ac.kr>
2022-04-01 23:21:52 +0900

    Feat: Complete chm_di

---
+++ Framework/chm_di/src/test/java/org/tukorea/test/MemberTest.java
@@ -0,0 +1,58 @@
+package org.tukorea.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.tukorea.di.domain.StudentVO;
+import org.tukorea.di.service.MemberService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/applicationContext.xml")
+
+
+public class MemberTest {
+	
+	@Autowired
+	MemberService memberService;
+	
+	@Test
+	public void testAddMember() throws Exception {
+		String strID = "hansol3";
+		StudentVO vo = new StudentVO();
+		vo.setId(strID);
+		vo.setPasswd(strID);
+		vo.setUsername(strID);
+		vo.setSnum(strID);
+		vo.setDepart(strID);
+		vo.setMobile(strID);
+		vo.setEmail(strID);
+		
+		memberService.addMember(vo);
+		
+		StudentVO member = memberService.readMember(strID);
+		System.out.println(member);
+	}
+	
+	@Test
+	public void testUpdateMember() throws Exception {
+		String strID = "hansol2";
+		StudentVO vo = new StudentVO();
+		vo.setId(strID);
+		vo.setPasswd(strID);
+		vo.setUsername(strID);
+		vo.setSnum(strID);
+		vo.setDepart(strID);
+		vo.setMobile(strID);
+		vo.setEmail(strID);
+		
+		memberService.updateMember(vo);
+		
+		StudentVO member = memberService.readMember(strID);
+		System.out.println(member);
+	}
+
+	
+	
+}
