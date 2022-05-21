abce8d209ce6a50185c36a4c429f06e725d2baf6
papavhub <pavi03@kpu.ac.kr>
2022-05-22 01:46:29 +0900

    Feat: org.tukorea.chm_free.domain

---
+++ Framework/chm_free/src/main/java/org/tukorea/chm_free/domain/PlaylistDetail_freeVO.java
@@ -0,0 +1,24 @@
+package org.tukorea.chm_free.domain;
+
+
+public class PlaylistDetail_freeVO {
+	private int playlistNumber;
+	private String playlistDetailSource;
+	
+	
+	public int getPlaylistNumber() {
+		return playlistNumber;
+	}
+	public void setPlaylistNumber(int playlistNumber) {
+		this.playlistNumber = playlistNumber;
+	}
+	public String getPlaylistDetailSource() {
+		return playlistDetailSource;
+	}
+	public void setPlaylistDetailSource(String playlistDetailSource) {
+		this.playlistDetailSource = playlistDetailSource;
+	}
+	
+	
+	
+}
---
+++ Framework/chm_free/src/main/java/org/tukorea/chm_free/domain/Playlist_freeVO.java
@@ -0,0 +1,59 @@
+package org.tukorea.chm_free.domain;
+
+
+public class Playlist_freeVO {
+	private int playlistNumber;
+	private String playlistName;
+	private String playlistDescribe;
+	private int playlistLikes;
+	private String playlistLink;
+	private String playlistPassword;
+	private String playlistPhoto;
+	
+	
+	public int getPlaylistNumber() {
+		return playlistNumber;
+	}
+	public void setPlaylistNumber(int playlistNumber) {
+		this.playlistNumber = playlistNumber;
+	}
+	public String getPlaylistName() {
+		return playlistName;
+	}
+	public void setPlaylistName(String playlistName) {
+		this.playlistName = playlistName;
+	}
+	public String getPlaylistDescribe() {
+		return playlistDescribe;
+	}
+	public void setPlaylistDescribe(String playlistDescribe) {
+		this.playlistDescribe = playlistDescribe;
+	}
+	public int getPlaylistLikes() {
+		return playlistLikes;
+	}
+	public void setPlaylistLikes(int playlistLikes) {
+		this.playlistLikes = playlistLikes;
+	}
+	public String getPlaylistLink() {
+		return playlistLink;
+	}
+	public void setPlaylistLink(String playlistLink) {
+		this.playlistLink = playlistLink;
+	}
+	public String getPlaylistPassword() {
+		return playlistPassword;
+	}
+	public void setPlaylistPassword(String playlistPassword) {
+		this.playlistPassword = playlistPassword;
+	}
+	public String getPlaylistPhoto() {
+		return playlistPhoto;
+	}
+	public void setPlaylistPhoto(String playlistPhoto) {
+		this.playlistPhoto = playlistPhoto;
+	}
+	
+	
+	
+}
